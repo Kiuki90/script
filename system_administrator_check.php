a0bb41bb2a5407eac2976d4ccda99642180b5db5
Gian Marco <82383254+Kiuki90@users.noreply.github.com>
2021-04-22 17:23:10 +0200

    Create system_administrator_check.php

---
+++ system_administrator_check.php
@@ -0,0 +1,61 @@
+<?php
+//
+// Script per monitorare il Cloud Server Ares
+// # (c) 2015 Gian Marco Chiuchiolo
+//
+
+// Creazione Array
+$checkram = array();
+$checkprocessi= array();
+$checkspace = array();
+$checkusers = array();
+
+// Controllo RAM disponibile
+exec("/usr/bin/free -m ", $checkram);
+
+// Controllo Processi in esecuzione
+exec("/bin/ps aux", $checkprocessi);
+
+// Controllo Spazio Disponibile
+exec("/bin/df -H", $checkspace);
+
+// Controllo Utenti connessi
+exec("/etc/alternatives/w", $checkusers);
+
+
+// Implosione array RAM in stringa
+$showram = implode("\n", $checkram);
+
+// Implosione array Processi in stringa
+$showprocessi = implode("\n", $checkprocessi);
+
+// Implosione array Space in stringa
+$showspace = implode("\n", $checkspace);
+
+// Implosione array Utenti in stringa
+$showusers = implode("\n", $checkusers);
+
+
+// Invio Mail
+$to      = 'g.chiuchiolo@neikos.eu';
+$subject = 'SYSTEM ADMINISTRATOR CHECK: CS ares.neikos.it';
+$message = "Controlli di sistema per monitorare il corretto funzionamento del Cloud Server:\n\n" .
+            "Controllo RAM:\n" . $showram . "\n\n" .
+            "Controllo SPAZIO:\n" . $showspace . "\n\n" .
+            "Controllo UTENTI:\n" . $showusers . "\n\n" .
+            "Controllo PROCESSI:\n" . $showprocessi . "\n\n" ;
+
+$headers = 'From: support@neikos.it' . "\r\n" .
+'Reply-To: g.chiuchiolo@neikos.eu' . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+
+$headers = 'MIME-Version: 1.0' . "\r\n";
+//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: support <support@neikos.it>' . "\r\n";
+$headers .= 'Reply-To: g.chiuchiolo@neikos.eu' . "\r\n" .
+$headers .= 'Content-Transfer-Encoding: base64' . "\r\n\r\n";
+$headers .= rtrim(chunk_split(base64_encode($message)));
+
+mail($to, $subject, "", $headers);
+
+// Fine
